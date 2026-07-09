53beba29f4a84d25774d79793523b51eb0642ca2
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2026-07-09 10:05:04 +0300

    feat: integrate morgan middleware for request logging in development environment

--- server.ts
+++ server.ts
@@ -1,9 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+import morgan from "morgan";
 
 dotenv.config({ path: "config.env" });
 
 const app = express();
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
