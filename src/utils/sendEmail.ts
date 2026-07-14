4b7f4b9ab3e8aefd244143e2b935872fd5de5e1b
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2026-07-14 16:12:10 +0300

    feat: implement email utility service using nodemailer

---
+++ src/utils/sendEmail.ts
@@ -0,0 +1,24 @@
+import nodemailer from "nodemailer";
+
+export async function sendEmail(options: any) {
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT
+      ? parseInt(process.env.EMAIL_PORT, 10)
+      : undefined,
+    secure: process.env.EMAIL_SECURE === "true",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  } as any);
+
+  const mailOptions = {
+    from: `E-Shop App <${process.env.EMAIL_USER}>`,
+    to: options.to,
+    subject: options.subject,
+    text: options.text,
+  };
+
+  await transporter.sendMail(mailOptions);
+}
