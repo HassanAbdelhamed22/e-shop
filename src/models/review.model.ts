c7c710b5e0dc95fe8fd59e3c9d531260f1dba333
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2026-07-15 16:24:32 +0300

    feat: create review model with schema and validation for products and users

---
+++ src/models/review.model.ts
@@ -0,0 +1,44 @@
+import mongoose, { Model, Schema } from "mongoose";
+import { IUser } from "../types";
+
+interface Review {
+  user: mongoose.Types.ObjectId | IUser;
+  rating: number;
+  title: string;
+  comment: string;
+  product: mongoose.Types.ObjectId;
+}
+
+const reviewSchema = new Schema<Review>(
+  {
+    title: {
+      type: String,
+      required: [true, "Review title is required"],
+      trim: true,
+    },
+    rating: {
+      type: Number,
+      required: [true, "Rating is required"],
+      min: [1, "Rating must be at least 1"],
+      max: [5, "Rating must be at most 5"],
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "Review must belong to a user"],
+    },
+    comment: {
+      type: String,
+    },
+    product: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      required: [true, "Review must belong to a product"],
+    },
+  },
+  { timestamps: true },
+);
+
+const Review: Model<Review> = mongoose.model<Review>("Review", reviewSchema);
+
+export default Review;
