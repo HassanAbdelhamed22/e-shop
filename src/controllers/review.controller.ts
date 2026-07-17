500429d9dff50c0f65000d159706610e000c5641
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2026-07-17 08:36:05 +0300

    feat(review): add controller middlewares for nested product reviews
    - Add `createFilterObject` middleware to filter reviews by product ID.
    - Add `setProductIdAndUserIdToBody` middleware to auto-populate product and user IDs from request context.

--- src/controllers/review.controller.ts
+++ src/controllers/review.controller.ts
@@ -1,5 +1,19 @@
+import type { NextFunction, Request, Response } from "express";
 import Review from "../models/review.model.ts";
 import * as controllerFactory from "./handlersFactory.ts";
+
+export const createFilterObject = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  let filterObject = {};
+  if (req.params.productId) {
+    filterObject = { product: req.params.productId };
+  }
+  req.filterObject = filterObject;
+  next();
+};
 
 // @desc    Get All Reviews
 // @route   GET /api/v1/reviews
@@ -10,6 +24,16 @@ export const getReviews = controllerFactory.getAll(Review, "Review");
 // @route   GET /api/v1/reviews/:id
 // @access  Public
 export const getReviewById = controllerFactory.getOne(Review);
+
+export const setProductIdAndUserIdToBody = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  if (!req.body.product) req.body.product = req.params.productId;
+  if (!req.body.user) req.body.user = req.user?._id;
+  next();
+};
 
 // @desc    Create Review
 // @route   POST /api/v1/reviews
