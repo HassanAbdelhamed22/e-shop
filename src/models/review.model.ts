93ec6cf18a0c427ef27f884b0555215f4c346298
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2026-07-17 09:39:25 +0300

    feat(review): implement average ratings and quantity calculation on save
    - Import Product model inside review model.
    - Add `calculateAverageRatingsAndQuantity` static method to review schema to aggregate ratings.
    - Update `ratingsAverage` and `ratingsQuantity` on Product model when aggregation finishes.
    - Add post-save hook to trigger calculations automatically when a review is saved.
    - Declare custom `ReviewModel` interface for TypeScript type-safety.
    feat(review): implement average ratings and quantity calculation on save
    
    - Import Product model inside review model.
    - Add `calculateAverageRatingsAndQuantity` static method to review schema to aggregate ratings.
    - Update `ratingsAverage` and `ratingsQuantity` on Product model when aggregation finishes.
    - Add post-save hook to trigger calculations automatically when a review is saved.
    - Declare custom `ReviewModel` interface for TypeScript type-safety.

--- src/models/review.model.ts
+++ src/models/review.model.ts
@@ -1,5 +1,6 @@
 import mongoose, { Model, Schema } from "mongoose";
 import type { IUser } from "../types/index.ts";
+import Product from "./product.model.ts";
 
 interface Review {
   user: mongoose.Types.ObjectId | IUser;
@@ -44,6 +45,49 @@ reviewSchema.pre(/find/i, function (this: any) {
   this.populate("user", "name email profileImage");
 });
 
-const Review: Model<Review> = mongoose.model<Review>("Review", reviewSchema);
+interface ReviewModel extends Model<Review> {
+  calculateAverageRatingsAndQuantity(
+    productId: mongoose.Types.ObjectId,
+  ): Promise<void>;
+}
+
+reviewSchema.statics.calculateAverageRatingsAndQuantity = async function (
+  this: mongoose.Model<any>,
+  productId: mongoose.Types.ObjectId,
+) {
+  const result = await this.aggregate([
+    {
+      $match: {
+        product: productId,
+      },
+    },
+    {
+      $group: {
+        _id: "$product",
+        avgRating: { $avg: "$rating" },
+        ratingsCount: { $sum: 1 },
+      },
+    },
+  ]);
+  if (result.length > 0) {
+    await Product.findByIdAndUpdate(productId, {
+      ratingsAverage: result[0].avgRating,
+      ratingsQuantity: result[0].ratingsCount,
+    });
+  } else {
+    await Product.findByIdAndUpdate(productId, {
+      ratingsAverage: 0,
+      ratingsQuantity: 0,
+    });
+  }
+};
+
+reviewSchema.post("save", async function () {
+  await (this.constructor as ReviewModel).calculateAverageRatingsAndQuantity(
+    this.product as any,
+  );
+});
+
+const Review = mongoose.model<Review, ReviewModel>("Review", reviewSchema);
 
 export default Review;
